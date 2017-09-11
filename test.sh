dc5e3c71772ab8bc44a12e941ba9b4900c88204b
Pierre de La Morinerie <kemenaran@gmail.com>
2017-09-11 14:44:53 +0530

    Vagrant: use a pre-packaged Yunohost box
    instead of installing Yunohost ourselves.

--- test.sh
+++ test.sh
@@ -10,7 +10,8 @@ set -e
 # Configuration constants
 APP_NAME="mattermost"
 TESTS_DIR="/home/vagrant/tests"
-APP_DIR="$TESTS_DIR/mattermost_ynh"
+APP_DIR="$TESTS_DIR/app"
+DOMAIN="ynh-tests.local"
 VM_ROOT_PASSWORD="alpine"
 YUNOHOST_ADMIN_PASSWORD="alpine"
 
@@ -81,16 +82,19 @@ function setup() {
     return
   fi
 
-  if (vagrant snapshot list | grep 'yunohost-2.4-pristine' > /dev/null); then
+  if (vagrant snapshot list | grep 'yunohost-jessie-pristine' > /dev/null); then
     echo "--- Restoring Vagrant snapshot ---"
-    vagrant snapshot restore yunohost-2.4-pristine
+    vagrant snapshot restore yunohost-jessie-pristine
   else
     echo "--- Provisioning Vagrant box ---"
     vagrant up --provision
     echo "--- Saving Vagrant snapshot ---"
-    vagrant snapshot save yunohost-2.4-pristine
+    vagrant snapshot save yunohost-jessie-pristine
   fi
 
+  # Copy the files inside the VM, rather than using the mounted
+  # files directly.
+  # This avoids tests screwing up our files if something goes wrong.
   echo "--- Copying app content into the box ---"
   if ! [ -d "$APP_DIR" ]; then
     _vagrant_ssh "mkdir -p '$TESTS_DIR'"
@@ -100,7 +104,7 @@ function setup() {
 
 function test_simple_install() {
   echo "--- Running simple installation test ---"
-  _vagrant_ssh "sudo yunohost app install '$APP_DIR' --args 'domain=mattermost-ynh.local&public_site=Yes&analytics=0' $VERBOSE_OPT"
+  _vagrant_ssh "sudo yunohost app install '$APP_DIR' --args 'domain=${DOMAIN}&public_site=Yes&analytics=0' $VERBOSE_OPT"
 }
 
 function test_simple_upgrade() {
