6adb7c6d3e529ab9d41948b95970a44a7924b032
Pierre de La Morinerie <kemenaran@gmail.com>
2017-10-16 10:02:16 +0530

    test: fix changes committed by mistake

--- test.sh
+++ test.sh
@@ -151,10 +151,10 @@ function teardown() {
 
 _parse_args $*
 setup
+test_package_check
 test_simple_install
-#test_simple_upgrade
-#test_simple_backup
-#test_simple_remove
-#test_simple_restore
-#test_package_check
+test_simple_upgrade
+test_simple_backup
+test_simple_remove
+test_simple_restore
 teardown
