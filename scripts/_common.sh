538d444e2c3790cd2cfdbabde9a5d9e7d4b2ae7a
ericgaspar <junk.eg@free.fr>
2021-01-10 19:39:28 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql apt-transport-https"
+pkg_dependencies="postgresql postgresql-contrib"
 
 #=================================================
 # PERSONAL HELPERS
