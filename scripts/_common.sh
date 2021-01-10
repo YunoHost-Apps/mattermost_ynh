be60ee9cb0d679c025a88214c3c85f8e5a4e4a37
ericgaspar <junk.eg@free.fr>
2021-01-10 22:11:56 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql postgresql-contrib"
+#pkg_dependencies="postgresql postgresql-contrib"
 
 #=================================================
 # PERSONAL HELPERS
