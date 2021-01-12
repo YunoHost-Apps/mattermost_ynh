c425aa5d9405182f616fc53bc97fbddc1854befb
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2021-01-12 10:33:02 +0100

    Arm arch. support (#201)
    * Add ARM sources
    * Switch to TEAM edition

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,6 +12,33 @@
 # EXPERIMENTAL HELPERS
 #=================================================
 
+# Check the architecture
+#
+# example: architecture=$(ynh_detect_arch)
+#
+# usage: ynh_detect_arch
+#
+# Requires YunoHost version 2.2.4 or higher.
+
+ynh_detect_arch(){
+        local architecture
+        if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
+                architecture="arm64"              
+        elif [ -n "$(uname -m | grep 64)" ]; then
+                architecture="x86-64"
+        elif [ -n "$(uname -m | grep armv7)" ]; then
+                architecture="arm"
+        elif [ -n "$(uname -m | grep armv6)" ]; then
+                architecture="arm"
+        elif [ -n "$(uname -m | grep armv5)" ]; then
+                architecture="arm"
+        else
+                architecture="unknown"
+        fi
+        echo $architecture
+}
+
+
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
