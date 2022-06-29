4e569aeecee2bef6bee71f59c893ac01a0664c48
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-06-29 10:29:17 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -27,8 +27,8 @@ mysql-to-pg() {
 		# Migrating from MySQL to PostgreSQL
 		pgloader mysql://mattermost:$mysql_db_pwd@localhost:3306/mattermost postgresql://mattermost:$db_pwd@localhost:5432/mattermost
 		
-		ynh_replace_string --match_string="\(\"DriverName\" *: \).*," --replace_string="\"DriverName\": \"postgres\"", --target_file="$final_path/config/config.json"
-		ynh_replace_string --match_string="\(\"DataSource\" *: \).*," --replace_string="\"DataSource\": \"postgres://$db_user:$db_pwd@localhost:5432/$db_name?sslmode=disable&connect_timeout=10\"", --target_file="$final_path/config/config.json"
+		ynh_replace_string --match_string="\(\"DriverName\" *: \).*," --replace_string="\"DriverName\": \"postgres\"," --target_file="$final_path/config/config.json"
+		ynh_replace_string --match_string="\(\"DataSource\" *: \).*," --replace_string="\"DataSource\": \"postgres://$db_user:$db_pwd@localhost:5432/$db_name?sslmode=disable&connect_timeout=10\"," --target_file="$final_path/config/config.json"
 
 		# Removinging MySQL database
 		ynh_mysql_remove_db --db_user=$db_user --db_name=$db_name
