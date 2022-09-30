34c2dca50a26f085ead1c61491fd810f782362d5
ericgaspar <junk.eg@free.fr>
2022-09-30 11:07:47 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -18,18 +18,18 @@ mysql-to-pg() {
 		ynh_script_progression --message="Migrating to PostgreSQL database..." --weight=10
 
 		mysql_db_pwd=$(ynh_app_setting_get --app=$app --key=mysqlpwd)
-		db_pwd=$(ynh_string_random --length=24)
+		postgresql_db_pwd=$(ynh_string_random --length=24)
 
 		ynh_psql_test_if_first_run
 		ynh_psql_setup_db --db_user=$db_user --db_name=$db_name --db_pwd=$db_pwd
-		db_pwd=$(ynh_app_setting_get --app=$app --key=db_pwd)
+		postgresql_db_pwd=$(ynh_app_setting_get --app=$app --key=postgresql_db_pwd)
 
 
 		# Migrating from MySQL to PostgreSQL
-		pgloader mysql://mattermost:$mysql_db_pwd@localhost:3306/mattermost postgresql://mattermost:$db_pwd@localhost:5432/mattermost
+		pgloader mysql://mattermost:$mysql_db_pwd@localhost:3306/mattermost postgresql://mattermost:$postgresql_db_pwd@localhost:5432/mattermost
 		
 		ynh_replace_string --match_string="\"DriverName\":*," --replace_string="\"DriverName\": \"postgres\"," --target_file="$final_path/config/config.json"
-		ynh_replace_string --match_string="\"DataSource\":*," --replace_string="\"DataSource\": \"postgres://$db_user:$db_pwd@localhost:5432/$db_name?sslmode=disable&connect_timeout=10\"," --target_file="$final_path/config/config.json"
+		ynh_replace_string --match_string="\"DataSource\":*," --replace_string="\"DataSource\": \"postgres://$db_user:$postgresql_db_pwd@localhost:5432/$db_name?sslmode=disable&connect_timeout=10\"," --target_file="$final_path/config/config.json"
 
 		#=================================================
 		# MODIFY A CONFIG FILE
